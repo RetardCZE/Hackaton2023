Added correct rotation reading for robot and world. Robot now turns to clicked position.

src/DataReader.py
@@ -53,6 +53,7 @@ from bosdyn.api.basic_command_pb2 import RobotCommandFeedbackStatus
 from bosdyn.client.estop import EstopClient, EstopEndpoint, EstopKeepAlive
 import io
 import numpy as np
+import copy
 import struct
 import matplotlib.pyplot as plt
 import plotly.express as px
@@ -322,149 +323,21 @@ class World:
     def show_world(self):
         return self.world[self.min_x:self.max_x, self.min_y:self.max_y]
 
-class Robot:
-    def __init__(self):
-        self.x = None
-        self.y = None
-        self.Pathing = False
-        sdk = bosdyn.client.create_standard_sdk('our-spot')
-        self.robot = sdk.create_robot('192.168.80.3')
-        bosdyn.client.util.authenticate(self.robot)
-        self.reader = DataReader(self.robot)
+    def euclidian_dist(self, p1, p2):
+        return np.sqrt(np.power(p2[0]-p1[0]) + np.power(p2[0]-p1[0]))
 
-        # Scatter plot
-        self.reader.reset()
-        self.reader.get_terrain_frames()
+    def get_path(self, goal):
+        if self.world[goal[0], goal[1]] == 255:
+            print("Cannot find path to obstacle")
 
-        position = self.reader.position()
-        self.world = World(position)
+        closed_world = copy.deepcopy(self.world)
+        mask_closed = closed_world[:, :] == 255
+        mask_open = closed_world[:, :] < 200
+        closed_world[mask_closed] = False
+        closed_world[mask_open] = True
 
-    def relative_move(self, dx, dy, angle, frame_name, robot_command_client, robot_state_client, stairs=False):
-        transforms = robot_state_client.get_robot_state().kinematic_state.transforms_snapshot
+        openQueue = [self.position]
 
-        # Build the transform for where we want the robot to be relative to where the body currently is.
-        body_tform_goal = math_helpers.SE2Pose(x=dx, y=dy, angle=angle)
-        # We do not want to command this goal in body frame because the body will move, thus shifting
-        # our goal. Instead, we transform this offset to get the goal position in the output frame
-        # (which will be either odom or vision).
-        out_tform_body = get_se2_a_tform_b(transforms, frame_name, BODY_FRAME_NAME)
-        out_tform_goal = out_tform_body * body_tform_goal
+        while openQueue:
+            current_cell = openQueue.pop(0)
 
-        # Command the robot to go to the goal point in the specified frame. The command will stop at the
-        # new position.
-        robot_cmd = RobotCommandBuilder.synchro_se2_trajectory_point_command(
-            goal_x=out_tform_goal.x, goal_y=out_tform_goal.y, goal_heading=out_tform_goal.angle,
-            frame_name=frame_name, params=RobotCommandBuilder.mobility_params(stair_hint=stairs))
-        end_time = 10.0
-        cmd_id = robot_command_client.robot_command(lease=None, command=robot_cmd,
-                                                    end_time_secs=time.time() + end_time)
-        # Wait until the robot has reached the goal.
-        while True:
-            feedback = robot_command_client.robot_command_feedback(cmd_id)
-            mobility_feedback = feedback.feedback.synchronized_feedback.mobility_command_feedback
-            if mobility_feedback.status != RobotCommandFeedbackStatus.STATUS_PROCESSING:
-                print('Failed to reach the goal')
-                return False
-            traj_feedback = mobility_feedback.se2_trajectory_feedback
-            if (traj_feedback.status == traj_feedback.STATUS_AT_GOAL and
-                    traj_feedback.body_movement_status == traj_feedback.BODY_STATUS_SETTLED):
-                print('Arrived at the goal.')
-                return True
-            time.sleep(1)
-
-        return True
-
-    def set_path(self):
-        print(self.world.coords2xy(self.x, self.y))
-        print(self.world.real_position)
-        print(self.world.position)
-        print(self.reader.position())
-        print(self.reader.getPosRot())
-        return
-        lease_client = self.robot.ensure_client(LeaseClient.default_service_name)
-        lease_client.take()
-        # Setup clients for the robot state and robot command services.
-        robot_state_client = self.robot.ensure_client(RobotStateClient.default_service_name)
-        robot_command_client = self.robot.ensure_client(RobotCommandClient.default_service_name)
-
-        estop_client = self.robot.ensure_client(EstopClient.default_service_name)
-        ep = EstopEndpoint(estop_client, 'name', 30)
-        ep.force_simple_setup()
-
-        # Begin periodic check-in between keep-alive and robot
-        estop_keep_alive = EstopKeepAlive(ep)
-        estop_keep_alive.allow()
-
-        with LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
-            # Power on the robot and stand it up.
-            self.robot.time_sync.wait_for_sync()
-            self.robot.power_on()
-            blocking_stand(robot_command_client)
-
-            try:
-                self.relative_move(0, 0, 1.57, ODOM_FRAME_NAME, robot_command_client, robot_state_client, stairs=False)
-            finally:
-                # Send a Stop at the end, regardless of what happened.
-                robot_command_client.robot_command(RobotCommandBuilder.stop_command())
-
-    def run(self):
-        q = -1
-        cv2.namedWindow('Terrain')
-        #cv2.setMouseCallback('Terrain', self.mouse_callback)
-        while q != 113:
-            grid = self.reader.get_terrain(0)
-
-            grid_mask = self.reader.get_terrain(1)
-            mask_off = np.mean(grid_mask[:, :, 2])
-            mask_free = grid_mask[:, :, 2] > mask_off
-
-            # mask_free = grid_mask[:, :, 2] == 1
-            mean = np.mean(grid[mask_free, 2])
-
-            mask_obstacles = grid[:, :, 2] > (mean + 0.15)
-            mask_hidden = grid_mask[:, :, 2] <= mask_off
-
-            grid[:, :, 2] = 255
-            grid[mask_hidden, 2] = 122
-            grid[mask_obstacles, 2] = 0
-
-            self.world.write_grid(grid)
-            # image = reader.show_terran_2d(grid)
-            # image = reader.show_terran_opencv(grid)
-            # stepper += 1
-            # if stepper == 6:
-            #    stepper = 0
-            self.world.eval_weights()
-            image = self.world.show_world()
-            p = self.world.position
-            x = p[0] - self.world.min_x
-            y = p[1] - self.world.min_y
-
-            color_mapped_image = cv2.applyColorMap((image).astype(np.uint8), cv2.COLORMAP_BONE)
-
-            # color_mapped_image = cv2.circle(color_mapped_image, (x, y), 10, (0, 255, 0), -1)  # Green circle, -1 for filled
-
-            image = cv2.resize(color_mapped_image, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
-
-            image = cv2.circle(image, (y * 4, x * 4), 10, (0, 255, 0), -1)  # Green circle, -1 for filled
-            image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-            cv2.imshow('Terrain', image)
-            q = cv2.waitKey(100)
-            # reader.reset()
-        cv2.destroyAllWindows()
-    def mouse_callback(self, event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            self.y = int(x/6) + self.world.min_x
-            self.x = int(y/6) + self.world.min_y
-            if self.world.show_world()[self.x-self.world.min_x, self.y-self.world.min_y] == 255:
-                self.set_path()
-                print(f"Clicked at FREE: ({self.x}, {self.y})")
-            else:
-                print(f"Clicked at OBSCURED: ({self.x}, {self.y})")
-
-if __name__ == "__main__":
-    os.environ['BOSDYN_CLIENT_USERNAME'] = 'user'
-    os.environ['BOSDYN_CLIENT_PASSWORD'] = 'rik36otsjn73'
-
-    r = Robot()
-    r.run()